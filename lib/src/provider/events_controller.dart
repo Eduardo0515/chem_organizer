c4a85b3d7211c5004c8d19c2c67a5370d8b78512
Carlos-Daniel260 <danielcaballerocc20@gmail.com>
2021-07-03 21:34:03 -0500

    identation

--- lib/src/provider/events_controller.dart
+++ lib/src/provider/events_controller.dart
@@ -36,9 +36,12 @@ class EventsController {
         t = doc['fecha'];
         date = t.toDate();
         if (auxEvents.containsKey(getJustDate(date))) {
-          auxEvents[getJustDate(date)]!.add(Event(doc.id, doc['nombre'], date, doc['categoria']));
+          auxEvents[getJustDate(date)]!
+              .add(Event(doc.id, doc['nombre'], date, doc['categoria']));
         } else {
-          auxEvents[getJustDate(date)] = [Event(doc.id, doc['nombre'], date, doc['categoria'])];
+          auxEvents[getJustDate(date)] = [
+            Event(doc.id, doc['nombre'], date, doc['categoria'])
+          ];
         }
       });
     });
